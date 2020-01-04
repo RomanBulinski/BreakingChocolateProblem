9b6b529be73af09abd28cf05f09becd5177178e8
rbulin <roman.bulinski@pentacomp.pl>
2020-01-04 07:34:53 +0100

    Improved code

--- src/main/java/Chocolate.java
+++ src/main/java/Chocolate.java
@@ -1,6 +1,9 @@
 public class Chocolate {
 
         public static int breakChocolate(int n, int m) {
+            if(m==0 || n== 0){
+                return 0;
+            }
             return n *(m-1) + (n-1);
         }
 
